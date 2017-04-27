faef76de1992cf9e2f409c2615cc9355d357cbdd
monilpatel <pmonil@vt.edu>
2017-04-27 16:17:32 -0400

    added kaz code
    add fragment with all options

--- TurboTask/app/src/main/java/com/example/monil/turbotask/AddTaskFragment.java
+++ TurboTask/app/src/main/java/com/example/monil/turbotask/AddTaskFragment.java
@@ -1,22 +1,27 @@
 package com.example.monil.turbotask;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -26,14 +31,14 @@ import java.util.Map;
 
 public class AddTaskFragment extends Fragment implements View.OnClickListener{
 
-    public static final String TAG_ADD_TASK = "add_task_fragment";
-    EditText inputTask;
-    Button saveTask;
-
-
+    public static String TAG_ADD_TASK = "add_task_fragment";
+    private EditText inputTask;
+    private Button saveTask;
+    private static EditText dueDateTxt;
+    private Spinner priority;
+    private Spinner classTag;
     private FirebaseUser user;
     private DatabaseReference dbRef;
-//    private DatabaseReference userTasksRef;
 
     public static AddTaskFragment newInstance() {
         AddTaskFragment fragment = new AddTaskFragment();
@@ -43,38 +48,58 @@ public class AddTaskFragment extends Fragment implements View.OnClickListener{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setRetainInstance(true);
-        Log.d("buttons", "add task fragment on create");
         dbRef = FirebaseDatabase.getInstance().getReference("data");
         user = FirebaseAuth.getInstance().getCurrentUser();
-//        userTasksRef = FirebaseDatabase.getInstance().getReference("data").child("user-tasks").child(user.getUid());
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        Log.d("buttons", "on create view");
-
         View v = inflater.inflate(R.layout.add_task_fragment, container, false);
+
         inputTask = (EditText)v.findViewById(R.id.inputTask);
         saveTask = (Button) v.findViewById(R.id.saveTask);
         saveTask.setOnClickListener(this);
+        dueDateTxt = (EditText) v.findViewById(R.id.dueDate);
+        dueDateTxt.setOnClickListener(this);
+
+        priority = (Spinner) v.findViewById(R.id.priority);
+        classTag = (Spinner) v.findViewById(R.id.classTag);
+        ArrayAdapter<CharSequence> priority_adapter = ArrayAdapter.createFromResource(getActivity(), R.array.priority_array, android.R.layout.simple_spinner_item);
+        priority_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        priority.setPrompt("Select your priority!");
+
+        priority.setAdapter(
+                new NothingSelectedSpinnerAdapter(
+                        priority_adapter,
+                        R.layout.contact_spinner_row_nothing_selected_priority,
+                        getActivity()));
+
+        ArrayAdapter<CharSequence> classTag_adapter = ArrayAdapter.createFromResource(getActivity(), R.array.class_array, android.R.layout.simple_spinner_item);
+        classTag_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        classTag.setPrompt("Select your class!");
+
+        classTag.setAdapter(
+                new NothingSelectedSpinnerAdapter(
+                        classTag_adapter,
+                        R.layout.contact_spinner_row_nothing_selected_classtag,
+                        getActivity()));
         return  v;
     }
 
     @Override
     public void onClick(View v) {
-        Log.d("buttons", v.getId() + "");
-        if(v.equals(saveTask)){
+        if(v.equals(dueDateTxt)) {
+            DialogFragment picker = new DatePickerFragment();
+            picker.show(getFragmentManager(), "datePicker");
+        }
+        else if(v.equals(saveTask)){
             Log.d("buttons", "task saved!" );
-            createNewTask(user.getUid(), inputTask.getText().toString(), "", "", 0);
-            //TODO: send back to task list
-//            postTask();
-
+            createNewTask(user.getUid(), inputTask.getText().toString(), dueDateTxt.getText().toString(),classTag.getSelectedItem().toString(),  Integer.parseInt(priority.getSelectedItem().toString() ));
         }
     }
+
 
     private void createNewTask(String uid, String name, String date, String className, int priority){
         String key = dbRef.child("tasks").push().getKey();
@@ -88,25 +113,29 @@ public class AddTaskFragment extends Fragment implements View.OnClickListener{
 
     }
 
-    private void postTask(){
-        final String uid = user.getUid();
-        FirebaseDatabase.getInstance().getReference().child("user-task").child(uid)
-                .addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        String nameText = inputTask.getText().toString();
-                        Task task = new Task(uid, nameText, "", "", 0);
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
 
-                        // Push the comment, it will appear in the list
-//                        userTasksRef.push().setValue(task);
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
 
-                    }
 
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-                    }
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            Calendar c = Calendar.getInstance();
+            c.set(year, month, day);
 
-                });
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            String formattedDate = sdf.format(c.getTime());
+            dueDateTxt.setText(new StringBuilder().append(month + 1).append("/").append(day).append("/").append(year));
+        }
     }
-
 }
